51cc886a9af4e5db98af8c501dacf84d8b6219f0
Koh <724537@cognizant.com>
2019-06-04 10:51:09 +0800

    Scheduler updated to use UTC

--- server/src/controllers/Scheduler.js
+++ server/src/controllers/Scheduler.js
@@ -8,26 +8,29 @@ const Scheduler = {
         const projectId = Config.PROJECT_ID;
         let sprintInfo = await db.getLatestSprintInfo(projectId);
         // let cronString = `4 * * * * *`;
-        let cronString = `0 0 * * *`;
+        let cronString = `5 0 * * *`;
         console.log(cronString);
         
         let CronJob = cron.CronJob;
         new CronJob(cronString, () => {
             console.log("Starting Scheduler...");
+            console.log(sprintInfo.data);
             Scheduler.scheduleNewSprint(sprintInfo.data);
         }, null, true, 'Asia/Singapore');
     },
 
     scheduleNewSprint(currentSprintInfo) {
         if (Date.now() >=  new Date(currentSprintInfo.reviewDate)) {
-            console.log(currentSprintInfo);
             const newSprintNo = currentSprintInfo.sprint_no + 1;
             let newSprintStartDate = new Date(currentSprintInfo.sprint_start_date);
             newSprintStartDate.setDate(newSprintStartDate.getDate() + 14);
+            newSprintStartDate = newSprintStartDate.toUTCString();
             let newSprintReleaseDate = new Date(currentSprintInfo.release_date);
             newSprintReleaseDate.setDate(newSprintReleaseDate.getDate() + 14);
+            newSprintReleaseDate= newSprintReleaseDate.toUTCString();
             let newSprintReviewDate = new Date(currentSprintInfo.review_date);
             newSprintReviewDate.setDate(newSprintReviewDate.getDate() + 14);
+            newSprintReviewDate = newSprintReviewDate.toUTCString();
 
             let historyJson = {
                 sprintNo: newSprintNo,
@@ -37,7 +40,8 @@ const Scheduler = {
                 reviewDate: newSprintReviewDate
             }
             
-            // db.updateHistory(historyJson)
+            console.log("Updating new sprint...")
+            db.updateHistory(historyJson)
             console.log(historyJson);
         }
     }
